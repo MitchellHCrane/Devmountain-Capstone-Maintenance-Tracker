0583e0e1e921f43451887a4a7743d190fbdd814e
Mitchell Crane <mitchellcrane@Mitchells-MacBook-Pro.local>
2021-09-20 19:08:45 -0600

    updated p.json for production

--- server/index.js
+++ server/index.js
@@ -5,6 +5,13 @@ const db = require("./db");
 
 const app = express();
 
+if (process.env.NODE_ENV === "production"{
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
