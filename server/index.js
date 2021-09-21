5780b4d5fff5db01c1740540815b002145e1fcba
Mitchell Crane <mitchellcrane@Mitchells-MacBook-Pro.local>
2021-09-20 19:37:36 -0600

    heroku script added

--- server/index.js
+++ server/index.js
@@ -29,5 +29,5 @@ app.delete("/records/:record_id", db.deleteRecord);
 // set port, listen for requests
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port #${PORT}.`);
 });
