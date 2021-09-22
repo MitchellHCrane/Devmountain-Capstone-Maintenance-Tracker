75806b5bc70f082e3bcfbb66fc2a90ced84fbe3b
Mitchell Crane <mitchellcrane@Mitchells-MacBook-Pro.local>
2021-09-22 13:22:22 -0600

    heroku testing

--- server/index.js
+++ server/index.js
@@ -16,11 +16,9 @@ app.use(
 
 app.use(cors());
 
-// test 17-21
-// app.use(express.static("public"));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "public", "index.html"));
-// });
+app.get('*', (req, res) => { //Its essentially a catchall. 
+  res.sendFile(path.join(__dirname, '../client/public/index.html'))
+})
 
 
 //API's
